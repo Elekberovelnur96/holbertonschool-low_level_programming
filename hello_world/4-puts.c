27a6f3f8f7244717a4f4ca770cb9f0dcbc8fa4e5
Elekberovelnur1996 <elnurelekberov264@gmail.com>
2025-06-14 16:12:02 +0000

    4-puts.c include <studio.h>

---
+++ hello_world/4-puts.c
@@ -0,0 +1,21 @@
+#!/bin/bash 
+#include <studio.h>
+
+/**
+ * main -Entery point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+  int main(void)
+	
+{
+       	puts ("\"Programming is like building a multilingual puzzle");
+        returun (0);	
+
+
+
+}
+
+
+
