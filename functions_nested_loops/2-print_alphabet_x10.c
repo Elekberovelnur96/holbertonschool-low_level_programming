41798a5302cc93d7feb54e02f6afb6896cd4c030
Elekberovelnur96 <elnurelekberov264@gmail.com>
2025-06-18 17:16:04 +0000

    kdjshl

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,23 +1,14 @@
 #include "main.h"
 
-/**
- * print_alphabet_x10 - Kiçik hərflərlə əlifbanı 10 dəfə çap edir.
- *
- * Təsvir: Bu funksiya 'a'-dan 'z'-yə qədər olan hərfləri və yeni sətr simvolunu
- * 10 dəfə ard-arda çap edir. Yalnız 2 dəfə _putchar funksiyası istifadə olunur.
- */
 void print_alphabet_x10(void)
 {
-	char alphabet[] = "abcdefghijklmnopqrstuvwxyz\n";
-	int i, j;
+    int i, j;
+    char c;
 
-	for (i = 0; i < 10; i++)
-	{
-		j = 0;
-		while (alphabet[j] != '\0')
-		{
-			_putchar(alphabet[j]);
-			j++;
-		}
-	}
+    for (i = 0; i < 10; i++)
+    {
+        for (j = 0, c = 'a'; j < 26; j++, c++)
+            _putchar(c);
+        _putchar('\n');
+    }
 }
