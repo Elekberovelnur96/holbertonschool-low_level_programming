85743b952ab5e2fecd427de41e3107832980e7df
Elekberovelnur96 <elnurlekberov264@gmail.com>
2025-06-20 15:59:33 +0000

    ejlwwefjlwejklwejklkljwe

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -8,8 +8,6 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
-void jack_bauer(void) ;
-void times_table(void);
-void jack_bauer(void);
+int print_last_digit(int n);
 #endif
 
