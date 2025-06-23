f77d870e6c1dbd15fae0b6a36ab6450bbb226db5
root <elnurelekbrov264@gmail.com>
2025-06-23 19:51:34 +0000

    4-print_most_numbers

--- more_functions_nested_loops/4-main.c
+++ more_functions_nested_loops/4-main.c
@@ -1,4 +1,4 @@
-#include "main."
+#include "main.h"
 
 /**
  * main - check the code
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-    print_numbers();
+    print_most_numbers();
     return (0);
 }
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -6,5 +6,6 @@ int _isdigit(int c);
 int mul(int a, int b);
 int main(void);
 int _putchar(char c);
+void print_most_numbers(void);
 #endif
 
