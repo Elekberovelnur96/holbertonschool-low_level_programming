2a1931154739135ce4f510c5644aa12422bc9f47
root <elnurelekbrov264@gmail.com>
2025-06-23 14:54:58 +0000

    open

--- more_functions_nested_loops/1-main.c
+++ more_functions_nested_loops/1-main.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * main - check the code
  *
@@ -9,7 +8,6 @@
 int main(void)
 {
     char c;
-
     c = '0';
     printf("%c: %d\n", c, _isdigit(c));
     c = 'a';
