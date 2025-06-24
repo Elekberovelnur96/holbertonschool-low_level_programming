0e111b043530da7e9c1a196933f4b168d88a56e2
Elekberovelnur96 <elnurelekberov264@gmail.com>
2025-06-24 20:15:54 +0000

    ejoiejiweojwio

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -9,24 +9,19 @@
 void print_triangle(int size)
 {
     int i, j;
-
     if (size <= 0)
     {
         _putchar('\n');
         return;
     }
-
     for (i = 1; i <= size; i++)
     {
         /* Print spaces first */
         for (j = 1; j <= size - i; j++)
             _putchar(' ');
-
         /* Then the '#' characters */
         for (j = 1; j <= i; j++)
             _putchar('#');
-
         _putchar('\n');
     }
 }
-
