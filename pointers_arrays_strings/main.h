f15077508349cf9b76e8dea073104ede4b31a99c
Elekberovelnur96 <elnurelekberov264@gmail.com>
2025-06-27 15:49:04 +0000

    jdklsmnklnjkln

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -15,4 +15,5 @@ char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strncat(char *dest, char *src, int n);
 int _putchar(char c);
+char *_strncpy(char *dest, char *src, int n);
 #endif
